4231cff61932066a07271bcfbf2dc31fd07f297e
Jason Cameron <dev.jasoncameron@gmail.com>
2025-09-09 19:39:59 -0500

    fixed. was not looking for the ttrpg(id), so i fixed that

--- src/main/java/com/smashingwizards/thewizardsbag_backend/controller/ItemSpellController.java
+++ src/main/java/com/smashingwizards/thewizardsbag_backend/controller/ItemSpellController.java
@@ -50,7 +50,4 @@ public class ItemSpellController {
         itemSpellService.deleteItemSpell(id);
     }
 
-    // ADDs
-
-
 }
--- src/main/java/com/smashingwizards/thewizardsbag_backend/repository/SpellRepository.java
+++ src/main/java/com/smashingwizards/thewizardsbag_backend/repository/SpellRepository.java
@@ -1,18 +1,17 @@
 package com.smashingwizards.thewizardsbag_backend.repository;
 
+import com.smashingwizards.thewizardsbag_backend.model.Item;
 import com.smashingwizards.thewizardsbag_backend.model.Spell;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface SpellRepository extends JpaRepository<Spell, Long> , JpaSpecificationExecutor<Spell> {
     // You can add custom queries here, like:
     // Optional<User> findByEmail(String email);
 
-    List<Spell> findByDescriptionContainingIgnoreCase(String description);
-    List<Spell> findAllByTtrpg(Long ttrpg);
+    List<Item> findAllByTtrpgId(Long ttrpgId);
 }
